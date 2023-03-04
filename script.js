691422ef0770821b8ceab92cdfef8d365c5358b5
prabh16 <114477888+prabh16@users.noreply.github.com>
2023-03-04 15:16:51 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -10,12 +10,14 @@ function search() {
     let prompt = queryElement.value;
     if (prompt) {
         ask.disabled = true;
+        promptElement.innerHTML = "";
+        responseElement.innerHTML = "";
         responsePElement.classList.add('loading');
         fetch(' https://api.openai.com/v1/completions', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
-                'Authorization': 'Bearer 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',
+                'Authorization': 'Bearer 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',
             },
             body: JSON.stringify({"model": "text-davinci-003", "prompt": prompt, "temperature": 0, "max_tokens": 7})
         })
