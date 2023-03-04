2d964e596fdf6bf1e38ab5f693d115650ba0c057
prabh16 <114477888+prabh16@users.noreply.github.com>
2023-03-04 16:04:04 +0530

    Eroor Handling

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ var queryElement = document.getElementById("query");
 var responseElement = document.getElementById("response");
 var promptElement = document.getElementById("prompt");
 var responsePElement = document.getElementById("response-p");
+var errorElement = document.getElementById("error");
 
 ask.addEventListener("click", search);
 
@@ -12,6 +13,7 @@ function search() {
         ask.disabled = true;
         promptElement.innerHTML = "";
         responseElement.innerHTML = "";
+        errorElement.innerHTML = "";
         responsePElement.classList.add('loading');
         fetch(' https://api.openai.com/v1/completions', {
             method: 'POST',
@@ -19,7 +21,7 @@ function search() {
                 'Content-Type': 'application/json',
                 'Authorization': 'Bearer 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',
             },
-            body: JSON.stringify({"model": "text-davinci-003", "prompt": prompt, "temperature": 0, "max_tokens": 7})
+            body: JSON.stringify({ "model": "text-davinci-003", "prompt": prompt, "temperature": 0, "max_tokens": 7 })
         })
             .then(response => response.json())
             .then(data => {
@@ -28,7 +30,13 @@ function search() {
                 promptElement.innerHTML = prompt;
                 responseElement.innerHTML = data.choices[0].text
             })
-            .catch(error => console.error(error));
+            .catch(
+                error => {
+                    console.error(error);
+                    promptElement.innerHTML = "";
+                    errorElement.innerHTML = "Ugh!!! " + "Something went wrong. Please try again later.";
+                }
+            );
     }
 
 }
